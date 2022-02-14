832766d5fe4e8114bc11f29550fed7659016a970
MathieuMenu <mathieu.menu02@gmail.com>
2022-02-14 21:15:38 +0100

    Projet de base SQL + Spring

---
+++ src/test/java/com/example/tutosql/TutosqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tutosql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutosqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
